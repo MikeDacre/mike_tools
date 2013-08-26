c7b12935545b026684df51a0ee2ffcd2bc157302
Mike Dacre <mike.dacre@gmail.com>
2013-08-26 10:47:32 -0700

    Created mike.py
    The file contains no funcitons yet, but it will soon contain most of my
    important python functions

---
+++ mike.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4
+"""
+#====================================================================================
+#
+#          FILE: mike (python 3)
+#        AUTHOR: Michael D Dacre, mike.dacre@gmail.com
+#  ORGANIZATION: Stanford University
+#       LICENSE: CC BY-NC-SA - Creative Commons Attribution-NonCommercial-ShareAlike
+#       VERSION: 0.1
+#       CREATED: 2013-08-26 10:39
+# Last modified: 2013-08-26 10:40
+#
+#   DESCRIPTION: General functions that I use in my scripts
+#
+#         USAGE: Run as a script or import as a module.  See '-h' or 'help' for usage
+#
+#====================================================================================
+"""
+
+
