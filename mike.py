5865e43b8458c019b5d34f791ba657ef92991744
Mike Dacre <mike.dacre@gmail.com>
2013-08-26 12:01:52 -0700

    Fixed bug in logfile handling

--- mike.py
+++ mike.py
@@ -10,7 +10,7 @@
 #       LICENSE: Open Source - Public - Do as you wish (no license) - Mike Dacre
 #       VERSION: 0.1
 #       CREATED: 2013-08-26 10:39
-# Last modified: 2013-08-26 11:44
+# Last modified: 2013-08-26 12:01
 #
 #   DESCRIPTION: General functions that I use in my scripts
 #
@@ -19,37 +19,46 @@
 #====================================================================================
 """
 
-def open_log(logfile):
+def open_log(logfile=''):
     """ Take either a string or a filehandle, detect if string.
         If string, open as file in append mode.
         If file, get name, close file, and reopen in append mode
         Return resulting file"""
-    if isinstance(logfile, str):
-        finalfile = open(logfile, 'a')
-    elif getattr(logfile, 'name') == '<stderr>' or getattr(logfile, 'name') == '<stdout>':
-        finalfile = logfile
-    elif not getattr(logfile, 'mode') == 'a':
-        logfile.close()
-        finalfile = open(logfile, 'a')
-    else:
-        raise Exception "logfile is not valid"
+    import sys
 
-    return logfile
-
-def logme(output, logfile):
-    """Print a string to logfile"""
-
-    output = str(output)
-
-    if isinstance(logfile, str):
-        with open(logfile, 'a') as outfile:
-            print(output, outfile)
-    else:
-        if getattr(logfile, 'name') == '<stderr>' or getattr(logfile, 'name') == '<stdout>':
+    if logfile:
+        if isinstance(logfile, str):
+            finalfile = open(logfile, 'a')
+        elif getattr(logfile, 'name') == '<stderr>' or getattr(logfile, 'name') == '<stdout>':
             finalfile = logfile
         elif not getattr(logfile, 'mode') == 'a':
             logfile.close()
             finalfile = open(logfile, 'a')
-        print(output, finalfile)
-        finalfile.close()
+        else:
+            raise Exception("logfile is not valid")
+    else:
+        finalfile = sys.stderr
+
+    return finalfile
+
+def logme(output, logfile=''):
+    """Print a string to logfile"""
+    import sys
+
+    output = str(output)
+
+    if logfile:
+        if isinstance(logfile, str):
+            with open(logfile, 'a') as outfile:
+                print(output, file=outfile)
+        else:
+            if getattr(logfile, 'name') == '<stderr>' or getattr(logfile, 'name') == '<stdout>':
+                finalfile = logfile
+            elif not getattr(logfile, 'mode') == 'a':
+                logfile.close()
+                finalfile = open(logfile, 'a')
+            print(output, file=finalfile)
+            finalfile.close()
+    else:
+        print(output, file=sys.stderr)
 
