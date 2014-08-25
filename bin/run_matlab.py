7cb56683fef706b7239b8f908740c9db7eafaf7a
Mike Dacre <mike.dacre@gmail.com>
2014-08-25 13:23:39 -0700

    Wrapper functions done, starting on main function

--- bin/run_matlab.py
+++ bin/run_matlab.py
@@ -14,7 +14,7 @@
 #       LICENSE: MIT License, Property of Stanford, Use as you wish                     #
 #       VERSION: 0.1                                                                    #
 #       CREATED: 2014-08-22 16:26                                                       #
-# Last modified: 2014-08-22 16:56
+# Last modified: 2014-08-25 13:23
 #                                                                                       #
 #   DESCRIPTION: Create a bunch of temporary matlab scripts to call some other          #
 #                matlab script and then submit to the cluster.                          #
@@ -28,54 +28,75 @@
 #                functions from http://j.mp/python_logme                                #
 #                                                                                       #
 #         USAGE: -p or --path allows the addition of multiple matlab paths              #
-#                -v or --variables is a list of variables to pass to the function       #
-#                    This list must be space separated. Each space separated item will  #
-#                    be run as a separate matlab job.                                   #
-#                    You may comma separate variables if you wish those variables to    #
-#                    passed as multiple arguments to a single function (e.g. not as     #
-#                    separate jobs.                                                     #
-#                                                                                       #
 #                <function> is a positional arg and is the name of the function to run  #
+#                                                                                       #
+#                STDIN: Variable list for matlab                                        #
+#                    This list must be space or newline separated. Each space separated #
+#                    item will be run as a separate matlab job.                         #         #
+#                    To provide multiple variables to the matlab function,              #
+#                    comma separate the variables on a single line.                     #
+#                                                                                       #
 #                                                                                       #
 #                Run as a script or import as a module.  See '-h' or 'help' for usage   #
 #                                                                                       #
 #=======================================================================================#
 """
-import logme
-from pbs_torque import job
+# Set to true to get pointlessly verbose output
+debug=True
+
+def submit_matlab_jobs(paths, variables, function, verbose=False, logfile=None):
+    """ Take a list of paths, a list of lists of variables, and a single Function
+        and submit one job for every list in the lists of variables (each item of
+        the second dimension will be submitted as an additional argument to the
+        matlab function. """
+    import logme
+    from pbs_torque import job
 
 def _get_args():
-    """Command Line Argument Parsing"""
-    import argparse, sys
+    """ Command Line Argument Parsing """
+    import argparse
 
     parser = argparse.ArgumentParser(
                  description=__doc__,
                  formatter_class=argparse.RawDescriptionHelpFormatter)
 
     # Optional Arguments
-    parser.add_argument('-p', '--path', nargs='?',
-                        help="Comma or space separated list of matlab paths")
-    parser.add_argument('-V', '--variables', nargs='?',
-                        help="Space separated list variables to pass to function")
-    parser.add_argument('-v', dest='verbose', help="Verbose output")
+    parser.add_argument('-p', '--path',
+                        help="Comma separated list of matlab paths")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose output")
 
     # Function name
     parser.add_argument('function',
                         help="Name of the matlab function to run")
 
     # Optional Files
-    parser.add_argument('-l', '--logfile', nargs='?',
+    parser.add_argument('-l', '--logfile',
                         help="Log File, Default STDERR (append mode)")
 
     return parser
 
 # Main function for direct running
 def main():
-    """Run directly"""
+    """ Run directly """
+    from sys import stdin
+    from os import path
+
     # Get commandline arguments
     parser = _get_args()
     args = parser.parse_args()
 
+    # Get variable list from STDIN
+    variables = [i.split(',') for i in stdin.read().rstrip().split('\n')]
+
+    # Split paths
+    paths = args.path.split(',')
+
+    if debug:
+        print(paths)
+        print(args.function)
+        print(variables)
+
+    submit_matlab_jobs(paths=paths, variables=variables, function=function, verbose=args.verbose, logfile=args.logfile)
 
 # The end
 if __name__ == '__main__':
