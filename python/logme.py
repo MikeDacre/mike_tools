bdf86970d8704cbb0f87e61efad8df4522b37924
Mike Dacre <mike.dacre@gmail.com>
2016-01-08 16:15:34 -0800

    Now works with python2 and python3

--- python/logme.py
+++ python/logme.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# vim:fenc=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4
-#
-# Copyright © Mike Dacre <mike.dacre@gmail.com>
-#
-# Distributed under terms of the MIT license
+# vim:set et sw=4 ts=4 tw=80:
 """
-#====================================================================================
-#
-#          FILE: logme (python 3)
-#        AUTHOR: Michael D Dacre, mike.dacre@gmail.com
-#  ORGANIZATION: Stanford University
-#       CREATED: 2015-03-03 11:41
-# Last modified: 2015-03-03 11:45
-#
-#   DESCRIPTION: Print a string to a logfile. logme can print to STDOUT or
-#                STDERR also:
-#                print_level: 1: print to STDOUT also
-#                print_level: 2: print to STDERR also
-#
-#         USAGE: import logme
-#                logme("Screw up!", sys.stderr, print_level=2
-#
-#====================================================================================
+================================================================================
+
+          FILE: logme
+        AUTHOR: Michael D Dacre, mike.dacre@gmail.com
+  ORGANIZATION: Stanford University
+       CREATED: 2015-03-03 11:41
+ Last modified: 2016-01-08 15:51
+
+   DESCRIPTION: Print a string to a logfile. logme can print to STDOUT or
+                STDERR also:
+                print_level: 1: print to STDOUT also
+                print_level: 2: print to STDERR also
+
+         USAGE: import logme
+                logme("Screw up!", sys.stderr, print_level=2
+
+================================================================================
 """
 import sys
+
 
 def open_log(logfile=''):
     """ Take either a string or a filehandle, detect if string.
@@ -35,7 +32,8 @@ def open_log(logfile=''):
     if logfile:
         if isinstance(logfile, str):
             finalfile = open(logfile, 'a')
-        elif getattr(logfile, 'name') == '<stderr>' or getattr(logfile, 'name') == '<stdout>':
+        elif getattr(logfile, 'name') == '<stderr>' \
+                or getattr(logfile, 'name') == '<stdout>':
             finalfile = logfile
         elif not getattr(logfile, 'mode') == 'a':
             logfile.close()
@@ -46,6 +44,7 @@ def open_log(logfile=''):
         finalfile = sys.stderr
 
     return finalfile
+
 
 def logme(output, logfile='', print_level=0):
     """ Print a string to logfile
@@ -63,28 +62,28 @@ def logme(output, logfile='', print_level=0):
     if logfile:
         if isinstance(logfile, str):
             with open(logfile, 'a') as outfile:
-                print(timeput, file=outfile)
+                outfile.write(timeput + '\n')
         elif getattr(logfile, 'name') == '<stderr>':
-            print(timeput, file=logfile)
+            logfile.write(timeput + '\n')
             stderr = True
         elif getattr(logfile, 'name') == '<stdout>':
-            print(timeput, file=logfile)
+            logfile.write(timeput + '\n')
             stdout = True
         elif getattr(logfile, 'mode') == 'a':
             if getattr(logfile, 'closed'):
                 with open(logfile.name, 'a') as outfile:
-                    print(timeput, file=outfile)
+                    outfile.write(timeput + '\n')
             else:
-                print(timeput, file=logfile)
+                logfile.write(timeput + '\n')
         else:
             logfile.close()
             with open(logfile, 'a') as outfile:
-                print(timeput, file=outfile)
+                outfile.write(timeput + '\n')
     else:
-        print(timeput, file=sys.stderr)
+        sys.stderr.write(timeput + '\n')
         stderr = True
 
     if print_level == 1 and not stdout:
-        print(output)
+        sys.stdout.write(output + '\n')
     elif print_level == 2 and not stderr:
-        print(output, file=sys.stderr)
+        sys.stderr.write(output + '\n')
